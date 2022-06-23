99e0cdb0e94b47c2f661fd5720bab2274dad8f1d
N_Coder <92726611+yashbhargava6740@users.noreply.github.com>
2022-06-23 17:45:04 +0530

    Infix_to_postFix_version3

---
+++ IPost_V3.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack 
+{
+    int top;
+    char *array;
+    int size;
+};
+
+char pop(struct stack *ptr) {
+    return ptr -> array[(ptr -> top)--];
+}
+
+void push(struct stack *ptr, char data) {
+    ptr -> array[++(ptr -> top)] = data;
+}
+
+char stackTop(struct stack *ptr) {
+    return ptr -> array[ptr -> top];
+}
+
+int isOperator(char a) {
+    if(a == '+' || a == '-' || a == '*' || a == '/' || a == '(' || a == ')' || a == '^') 
+        return 1;
+    return 0;
+} 
+
+int precedence(char a) {
+    static int x = 3;
+    if(a == '^') 
+        return x++;
+    if(a == '*' || a == '/')
+        return 2;
+    if(a == '+' || a == '-')
+        return 1; 
+    return 0;
+}
+
+char *infixToPostfix(char a[]) {
+    struct stack *st = (struct stack *)malloc(sizeof(struct stack));
+    st -> top = -1;
+    st -> size = 100;
+    st -> array = (char *)malloc(st -> size * sizeof(char));
+    int ln = 0, i = 0, ind = 0;
+    while(ln++, a[ln]);
+    char *postFix = (char *)malloc((ln+1) * sizeof(char));
+    for(i; a[i]; i++) {
+        if(!isOperator(a[i])) {
+            postFix[ind++] = a[i];
+        }
+        else {
+            if((a[i] == '(' || (precedence(stackTop(st)) < precedence(a[i]))) && a[i] != ')') {
+                push(st, a[i]);
+            }
+            else {
+                if(a[i] == ')') {
+                    while(1) {
+                        if(stackTop(st) == '(') {
+                            pop(st);
+                            break;
+                        }
+                        else {
+                            postFix[ind++] = pop(st);
+                        } 
+                    }
+                }
+                else {
+                    while(precedence(stackTop(st)) >= precedence(a[i])) {
+                        postFix[ind++] = pop(st);
+                    }
+                    push(st,a[i]);
+                }
+            }
+        }
+    }
+    while(st -> top != -1) {
+        if(stackTop(st) != '(')
+            postFix[ind++] = pop(st);
+        else
+            pop(st);
+    }
+        
+    postFix[ind++] = '\0';
+    return postFix;
+}
+
+
+void main() {
+    char string[1000];
+    scanf("%[^\n]%*c", string);
+    char *out = infixToPostfix(string);
+    printf("%s", out);
+}
