5a414c4d18797f3d2470d42e3a64d58d4d7dd632
N_Coder <92726611+yashbhargava6740@users.noreply.github.com>
2022-06-25 23:32:01 +0530

    Update Prime_Number_with_recursion.c

--- Prime_Number_with_recursion.c
+++ Prime_Number_with_recursion.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+// First Method
+/*int checkPrime(int nm) {
+    static int divisor = 2;
+    if((nm % divisor == 0 || nm == 1) && nm != 2) 
+        return 0;
+    if(divisor >= nm / 2 || nm == 2) 
+        return 1;
+    divisor++;
+    return checkPrime(nm);
+}*/
+
+
+// Second Method
 int checkPrime(int nm, int divisor) {
     if((nm % divisor == 0 || nm == 1) && nm != 2) 
         return 0;
@@ -8,6 +21,7 @@ int checkPrime(int nm, int divisor) {
     return checkPrime(nm, divisor+1);
 }
 
+// Main Function
 void main() {
     int n;
     printf("Enter Number:- ");
